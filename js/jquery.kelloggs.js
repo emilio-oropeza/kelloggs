b32f0026da4411586008f61880bd70a3f33ca37a
Vocho <vocho@underdog.gs>
2015-05-27 16:31:27 -0500

    g
    gg

--- js/jquery.kelloggs.js
+++ js/jquery.kelloggs.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", callback, false);
 		
 function callback(){
-    var traqball_2 = new Traqball({stage: "box1", axis: [0,1,0], angle: 0.7});
-    //var traqball_2 = new Traqball({stage: "box1", axis: [0,1,0], angle: 0.7, limitAxxis: "y"});
+    //var traqball_2 = new Traqball({stage: "box1", axis: [0,1,0], angle: 0.7});
+    var traqball_2 = new Traqball({stage: "box1", axis: [0,1,0], angle: 0.7, limitAxxis: "y"});
 }
 
