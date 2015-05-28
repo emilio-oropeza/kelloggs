4dfbc740a251f66b348281cda330791fd927a005
Vocho <vocho@underdog.gs>
2015-05-28 18:17:23 -0500

    agregado el showroom
    showroom

--- js/jquery.kelloggs.js
+++ js/jquery.kelloggs.js
@@ -1,3 +1,14 @@
+var videos = [
+	"UCF-IEMAEdw",
+	"exHGCSWc-rI",
+	"JVeih0R_06A",
+	"d3wplp7fLsU"
+];
+
+
+
+
+
 window.addEventListener("load", callback, false);
 		
 function callback(){
@@ -5,3 +16,31 @@ function callback(){
     var traqball_2 = new Traqball({stage: "box1", axis: [0,1,0], angle: 0.7, limitAxxis: "y"});
 }
 
+$(document).ready(function(){
+	var width = $(".video").width();
+	var height = $(".video").height();
+
+	$(window).resize(function(){
+		width = $(".video").width();
+		height = $(".video").height();
+	});
+
+	$(".videos").each(function(key, val){
+		if(key < videos.length){
+			$(this).click(function(){
+				$("#video_yt").show();
+				autoplay(videos[key]);
+			});
+		}	
+	});
+
+	$("#close").click(function(){
+		$("#video_yt_cont").html('');
+		$("#video_yt").hide();
+	});
+});
+
+function autoplay(vcode){
+	"use strict";
+  	$("#video_yt_cont").html('<iframe width="100%" height="100%"  src="https://www.youtube.com/embed/'+vcode+'?autoplay=1&loop=1&rel=0&wmode=transparent" frameborder="0" allowfullscreen wmode="Opaque"></iframe>');
+}
