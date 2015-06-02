ea0f0108384efcb227f62144b8df64b8eec77900
Vocho <vocho@underdog.gs>
2015-06-02 09:50:32 -0500

    fefef
    efrfef

--- js/jquery.kelloggs.js
+++ js/jquery.kelloggs.js
@@ -12,8 +12,8 @@ var videos = [
 window.addEventListener("load", callback, false);
 		
 function callback(){
-    var traqball_2 = new Traqball({stage: "box1", axis: [0,1,0], angle: 0.7});
-    //var traqball_2 = new Traqball({stage: "box1", axis: [0,1,0], angle: 0.7, limitAxxis: "y"});
+    //var traqball_2 = new Traqball({stage: "box1", axis: [0,1,0], angle: 0.7});
+    var traqball_2 = new Traqball({stage: "box1", axis: [0,1,0], angle: 0.7, limitAxxis: "y"});
 }
 
 $(document).ready(function(){
